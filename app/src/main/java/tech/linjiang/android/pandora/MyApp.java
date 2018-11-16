e3b0460f8e9f1195377b366bfc37d474a306a4c6
linjiang <y837979117@gmail.com>
2018-11-16 09:54:11 +0800

    :beers: enable StrictMode in demo

--- app/src/main/java/tech/linjiang/android/pandora/MyApp.java
+++ app/src/main/java/tech/linjiang/android/pandora/MyApp.java
@@ -2,9 +2,9 @@ package tech.linjiang.android.pandora;
 
 import android.app.Application;
 import android.os.Build;
+import android.os.StrictMode;
 
 import tech.linjiang.android.pandora.db.StoreDatabase;
-import tech.linjiang.android.pandora.net.GsonFormatterImpl;
 import tech.linjiang.android.pandora.net.JsonFormatterImpl;
 import tech.linjiang.pandora.Pandora;
 
@@ -20,6 +20,7 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         mThis = this;
+        strictMode();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             // the store.db and testAllType.xml if exist will be moved to the Device encrypted storage area.
             createDeviceProtectedStorageContext().moveDatabaseFrom(this, StoreDatabase.NAME);
@@ -33,5 +34,19 @@ public class MyApp extends Application {
 
     public static Application getContext() {
         return mThis;
+    }
+
+    private void strictMode() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyDialog()
+                .penaltyLog()
+                .penaltyFlashScreen()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectAll()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
     }
 }
